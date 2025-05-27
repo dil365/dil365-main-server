33c69013acb6861bb7a827a4f2b96a0c5bc29f0b
Vugar Safarzada <vuqarsfrzad@gmail.com>
2025-05-27 11:37:33 +0400

    Untrack ignored files.

--- dist/src/common/user_details/dto/create-user_detail.dto.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class CreateUserDetailDto {
-    user_id?: number;
-    first_name: string;
-    last_name: string;
-    birthdate?: Date;
-}
--- dist/src/guards/auth.guard.d.ts
+++
@@ -1,7 +0,0 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { TokenSessionsService } from 'src/common/token_sessions/token_sessions.service';
-export declare class AuthGuard implements CanActivate {
-    private session;
-    constructor(session: TokenSessionsService);
-    canActivate(context: ExecutionContext): Promise<boolean>;
-}
